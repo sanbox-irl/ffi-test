a9adf99d777e662429219cdd3a2fed180d1f235c
Jack OntheGoMac <jjspira@gmail.com>
2022-01-01 21:49:06 -0500

    init

---
+++ objects/Object1/Create_0.gml
@@ -0,0 +1,3 @@
+var o = test(102.0, 10.0);
+
+show_debug_message(o);
---
+++ target/vm/output/install_dmg.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set +x
+
+YYInstallDir=/Users/jjspira/gamemakerstudio2/GMS2MAC/ffi-test
+YYDMGName=Mac-YoYoRunner-Release.dmg
+YYDmgMD5=51ab46d3c43c375b86bc3d3874c06b2b 
+YYAppStore=0
+
+if [ ! -d "$YYInstallDir" ]; then
+	mkdir -p "$YYInstallDir"
+fi
+
+# Determine if we need to copy the runner over to this Mac???
+YYCopyDMG=1
+if [ -d "$YYInstallDir/YoYo Runner.app" ]; then
+	if [ -d "$YYInstallDir/.yoyo" ]; then
+		#echo "$YYInstallDir/.yoyo" exists
+		if [ -f "$YYInstallDir/.yoyo/dmg.info" ]; then
+			#echo "$YYInstallDir/.yoyo/dmg.info" exists
+			YYStoredMD5=`cat $YYInstallDir/.yoyo/dmg.info`
+			# If the MD5's match then everything is good
+			if [ "$YYDmgMD5" == "$YYStoredMD5" ]; then
+				YYCopyDMG=0
+			fi
+		fi
+	fi
+fi
+
+if [ -d "$YYInstallDir/YoYo Runner.app" ]; then
+	if [ ! -f "$YYInstallDir/YoYo Runner.app/Contents/Frameworks/libYoYoGamepad.dylib" ]; then
+		if [ $YYAppStore == "0" ]; then
+			YYCopyDMG=1
+		fi
+	fi
+
+	if [ ! -f "$YYInstallDir/YoYo Runner.app/Contents/Frameworks/libYoYoIAP.dylib" ]; then
+		if [ $YYAppStore == "1" ]; then
+			YYCopyDMG=1
+		fi
+	fi
+fi
+
+if [ "$YYCopyDMG" == "1" ]; then
+	# create the install directory if it does not exist
+	[ -d "$YYInstallDir" ] || mkdir -p $YYInstallDir
+	# if the DMG exists
+	if [ -f "$YYInstallDir/$YYDMGName" ]; then
+		
+		# Remove any app that may be present
+		rm -rf "$YYInstallDir/YoYo Runner.app"
+
+		# mount the DMG and copy the application over then update
+		hdiutil mount -quiet $YYInstallDir/$YYDMGName
+		cp -R "/Volumes/YoYo Runner/YoYo Runner.app" $YYInstallDir
+		hdiutil unmount -quiet  "/Volumes/YoYo Runner/"
+
+		if [ $YYAppStore == "1" ]; then
+			rm -f "$YYInstallDir/YoYo Runner.app/Contents/Frameworks/libYoYoGamepad.dylib"			
+		else
+			rm -f "$YYInstallDir/YoYo Runner.app/Contents/Frameworks/libYoYoIAP.dylib"
+		fi
+
+		# record the MD5 
+		[ -d "$YYInstallDir/.yoyo" ] || mkdir -p $YYInstallDir/.yoyo
+		md5 -q $YYInstallDir/$YYDMGName >$YYInstallDir/.yoyo/dmg.info
+		rm -rf $YYInstallDir/$YYDMGName
+
+		# and done
+		echo Done
+
+	else
+		echo CopyDMG
+	fi
+else
+	if [ $YYAppStore == "1" ]; then
+		rm -f "$YYInstallDir/YoYo Runner.app/Contents/Frameworks/libYoYoGamepad.dylib"
+	else
+		rm -f "$YYInstallDir/YoYo Runner.app/Contents/Frameworks/libYoYoIAP.dylib"
+	fi
+
+	echo Done
+fi
+
+
+
+
